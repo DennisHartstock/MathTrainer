3fad52e5a8f7d8d09409a40aaf20cbe882fb2d27
DennsHartstock <dennis.hartstock@web.de>
2022-11-01 06:41:31 +0100

    add exercise layout

--- app/src/main/java/com/example/mathtrainer/MainActivity.kt
+++ app/src/main/java/com/example/mathtrainer/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.example.mathtrainer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tvFirstArg: TextView
+    private lateinit var tvMathSign: TextView
+    private lateinit var tvSecondArg: TextView
+    private lateinit var tvEqualSign: TextView
+    private lateinit var etResult: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvFirstArg = findViewById(R.id.tvFirstArg)
+        tvMathSign = findViewById(R.id.tvMathSign)
+        tvSecondArg = findViewById(R.id.tvSecondArg)
+        tvEqualSign = findViewById(R.id.tvEqualSign)
+        etResult = findViewById(R.id.etResult)
+
+        buildExercise()
+//        getResult()
+    }
+
+    private fun buildExercise() {
+        val firstArg = (0 until 2).random()
+        val secondArg = (0 until 10).random()
+
+        tvFirstArg.text = firstArg.toString()
+        tvMathSign.text = "X"
+        tvSecondArg.text = secondArg.toString()
+        tvEqualSign.text = "="
+    }
+
+    private fun getResult() {
+        val myResult = etResult.text.toString().toInt()
+        val result = tvFirstArg.text.toString().toInt() * tvSecondArg.text.toString().toInt()
+        if (myResult == result) {
+            tvFirstArg.text = (0 until 2).random().toString()
+            tvSecondArg.text = (0 until 10).random().toString()
+        }
     }
 }
